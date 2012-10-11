efcddc5788a6fcf8cec55b5808e15e1d12cf86db
Kagami Hiiragi <kagami@genshiken.org>
2012-10-11 22:06:55 +0400

    Improve monad interface

--- src/Network/XMPP/Monad.hs
+++ src/Network/XMPP/Monad.hs
@@ -2,6 +2,7 @@ module Network.XMPP.Monad
     ( XMPP
     , initXMPP
     , runXMPP
+    , runXMPPLoop
     , XMPPState
     , StanzaPredicate
     , StanzaHandler
@@ -14,7 +15,7 @@ import Control.Monad (when)
 import Control.Monad.Reader (ReaderT, runReaderT, ask)
 import Control.Monad.Trans (MonadIO, liftIO)
 import Control.Concurrent.STM (atomically, TVar, newTVarIO, readTVarIO,
-                               writeTVar, modifyTVar')
+                               modifyTVar')
 import qualified Data.Map as M
 
 import Network.XMPP.XML (XMLElem)
@@ -31,10 +32,9 @@ type StanzaHandler = XMLElem -> XMPP ()
 -- (created on handler addition).
 type XMPPHandlers = M.Map Unique XMPPHandler
 
--- | State which we can read and modify inside a monad.
+-- | State which we can read and modify inside monad.
 data XMPPState = forall c. XMPPConnection c => XMPPState
-    { stateIsMainLoop :: TVar Bool
-    , stateConnection :: c
+    { stateConnection :: c
     , stateHandlers :: TVar XMPPHandlers
     }
 
@@ -45,38 +45,40 @@ newtype XMPP a = XMPP { unXMPP :: ReaderT XMPPState IO a }
 -- | Initialize monad state.
 initXMPP :: XMPPConnection c => c -> IO XMPPState
 initXMPP c = do
-    isMain <- newTVarIO False
     handlers <- newTVarIO M.empty
-    return $ XMPPState isMain c handlers
+    return $ XMPPState c handlers
 
--- | Run a function inside the XMPP monad using initialized state.
+-- | Run function inside the XMPP monad.
 runXMPP :: XMPPState -> XMPP () -> IO ()
-runXMPP state@(XMPPState { .. }) m = do
-    isMain <- readTVarIO stateIsMainLoop
-    when (not isMain) $
-        atomically $ writeTVar stateIsMainLoop True
-    runReaderT (unXMPP m) state
-    when isMain $
-        runXMPPLoop state []
+runXMPP state m = runReaderT (unXMPP m) state
 
-runXMPPLoop :: XMPPState -> [XMLElem] -> IO ()
-runXMPPLoop state@(XMPPState { .. }) [] = do
+-- | Run function inside the XMPP monad. After that, keep looping as
+-- long as there are handlers left waiting for incoming stanzas from
+-- XMPP connection.
+-- Note what this function must be called only once in the main thread.
+-- Use 'runXMPP' with the same state in other threads for working with
+-- XMPP (e.g. start worker thread and send message when it's done).
+runXMPPLoop :: XMPPState -> XMPP () -> IO ()
+runXMPPLoop state m = do
+    runXMPP state m
+    runXMPPLoop' state []
+
+runXMPPLoop' :: XMPPState -> [XMLElem] -> IO ()
+runXMPPLoop' state@(XMPPState { .. }) [] = do
     handlers <- readTVarIO stateHandlers
     when (not $ M.null handlers) $
-        getStanzas stateConnection >>= runXMPPLoop state
-runXMPPLoop state@(XMPPState { .. }) (stanza:stanzas) = do
+        getStanzas stateConnection >>= runXMPPLoop' state
+runXMPPLoop' state@(XMPPState { .. }) (stanza:stanzas) = do
     handlers <- readTVarIO stateHandlers
     case findHandler stanza handlers of
         Just (_, Catch _ stanzaHandler) ->
-            run (stanzaHandler stanza)
+            runXMPP state (stanzaHandler stanza)
         Just (uniq, CatchOnce _ stanzaHandler) -> do
             atomically $ modifyTVar' stateHandlers (M.delete uniq)
-            run (stanzaHandler stanza)
+            runXMPP state (stanzaHandler stanza)
         Nothing ->
             return ()
-    runXMPPLoop state stanzas
-  where
-    run m = runReaderT (unXMPP m) state
+    runXMPPLoop' state stanzas
 
 -- | Find handler for the given stanza based on predicates.
 findHandler :: XMLElem -> XMPPHandlers -> Maybe (Unique, XMPPHandler)
